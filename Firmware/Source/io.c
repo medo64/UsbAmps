a3a4a3fb043832530dfde945b42cebda57344d28
Josip Medved <jmedved@jmedved.com>
2014-06-25 23:22:43 -0700

    Refactoring io.

--- Firmware/Source/io.c
+++ Firmware/Source/io.c
@@ -1,28 +1,8 @@
 #include <pic.h>
-
-#define DSHORT_TRIS     TRISC1
-#define DSHORT_LAT      LATC1
-#define BACKLIGHT_TRIS  TRISC0
+#include "io.h"
 
 
 void io_init() {
-    DSHORT_TRIS = 0; //C1 is an output
-    BACKLIGHT_TRIS = 1;
-}
-
-void io_dshort_on() {
-    DSHORT_LAT = 1;
-}
-
-
-void io_backlight_on() {
-    BACKLIGHT_TRIS = 0;
-}
-
-void io_backlight_off() {
-    BACKLIGHT_TRIS = 1;
-}
-
-void io_backlight_toggle() {
-    BACKLIGHT_TRIS = !BACKLIGHT_TRIS;
+    IO_DSHORT_TRIS = 0; //C1 is an output
+    IO_BACKLIGHT_TRIS = 1;
 }
--- Firmware/Source/io.h
+++ Firmware/Source/io.h
@@ -1,12 +1,17 @@
 #ifndef IO_H
 #define IO_H
 
-void io_init();
+#define IO_DSHORT_TRIS     TRISC1
+#define IO_DSHORT_LAT      LATC1
+#define IO_BACKLIGHT_TRIS  TRISC0
 
-void io_dshort_on();
 
-void io_backlight_on();
-void io_backlight_off();
-void io_backlight_toggle();
+void io_init(void);
+
+#define io_dshort_on()         { IO_DSHORT_LAT = 1; }
+
+#define io_backlight_on()      { IO_BACKLIGHT_TRIS = 0; }
+#define io_backlight_off()     { IO_BACKLIGHT_TRIS = 1; }
+#define io_backlight_toggle()  { IO_BACKLIGHT_TRIS = !IO_BACKLIGHT_TRIS; }
 
 #endif
