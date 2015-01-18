019e8ab2ac96a5c37e69e8cd6b4c54ad7950c783
Josip Medved <jmedved@jmedved.com>
2015-01-18 09:30:01 -0800

    Increased calibration tolerance a bit.

--- Firmware/Source/calibrate.c
+++ Firmware/Source/calibrate.c
@@ -7,7 +7,7 @@
 #include "config.h"
 
 
-#define CURRENT_OFFSET_MAX     5 //offset must be less than 5 (~10 mA)
+#define CURRENT_OFFSET_MAX     5 //offset must be less or equal to 5 (~10 mA)
 #define VERIFY_COUNT        2000
 #define VERIFY_TOLERANCE       5
 
@@ -16,7 +16,7 @@ void calibrate() {
     uint16_t oldAdcCurrentOffset = settings_getAdcCurrentOffset();
     uint16_t newAdcCurrentOffset = UINT16_MAX;
 
-    for (uint16_t i = 0; i < CURRENT_OFFSET_MAX; i++) {
+    for (uint16_t i = 0; i <= CURRENT_OFFSET_MAX; i++) {
         uint16_t invalidCount = 0;
         for (uint16_t j = 0; j < VERIFY_COUNT; j++) {
             clrwdt();
